5ea5d08b88eb3adcfedef3c6950baf386986d070
chicho <chicho@upn.pe>
2025-04-08 19:06:04 -0500

    Semana3 V1.0

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,7 +10,19 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo"); //lee la linea
+            int a, b, resultado;
 
+            Console.Write("Ingrese primer valor");
+            /*a = Convert.ToInt32(Console.ReadLine());*/  a= int.Parse(Console.ReadLine()); // una forma de convertir el valor
+            Console.Write("Ingrese segundo numero");
+            b = Convert.ToInt32(Console.ReadLine());
+            resultado = a + b;
+            Console.WriteLine("LAsuma es " + resultado);
+
+            
+
+            //Console.ReadKey(); //DEtiene el programa hasta que se presione una tecla
         }
     }
 }
