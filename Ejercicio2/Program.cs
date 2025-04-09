85dae5f3bd240075d7d09450cda71bd902d83467
programador: Anthony <Tony2102@upn.pe>
2025-04-09 15:17:20 -0500

    Semana3 V1.3

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -8,8 +8,28 @@ namespace Ejercicio2
 {
     class Program
     {
+        /* ULTIMO EJERCICIO DE LA PRACTICA*/
         static void Main(string[] args)
         {
+            int a, b, c;
+            double raizCuadrada;
+
+            Console.WriteLine("Ingrese valor a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese valor b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese valor c: ");
+            c = int.Parse(Console.ReadLine());
+
+            /* logica*/
+
+            raizCuadrada = Math.Sqrt((b * b) - (4*(a * c)));
+
+            Console.WriteLine("el resultado es " + raizCuadrada);
+
+            /* Resultado*/
+
+
         }
     }
 }
