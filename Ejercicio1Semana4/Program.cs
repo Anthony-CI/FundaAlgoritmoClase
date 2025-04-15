1679329616e9dab3261931cd0af4b303143fb5da
chicho <chicho@upn.pe>
2025-04-15 17:57:26 -0500

    Semana 4

---
+++ Ejercicio1Semana4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Semana4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Ingrese el primer número:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            b = int.Parse(Console.ReadLine());
+            if (a>b)
+            {
+                int cambio;
+
+                cambio = a;
+                a = b;
+                b = cambio;
+                
+                Console.WriteLine("a es " + a);
+                Console.WriteLine("b es " + b);
+            }
+        }
+    }
+}
---
+++ Ejercicio2Semana4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Semana4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            int p, s;
+            Console.WriteLine("Ingrese el primer número:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            b = int.Parse(Console.ReadLine());
+            /* Console.WriteLine("Ingrese el tercer número:");
+             c = int.Parse(Console.ReadLine()); */
+
+            if (a > b)
+            {
+                s = a;
+                p = b;
+            }
+            else
+            {
+                s = b;
+                p = a;
+            }
+
+            Console.WriteLine("orden " + p + " " +s);
+
+
+        }
+    }
+}
+    
+           
