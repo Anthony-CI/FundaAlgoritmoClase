8c318f8e2b3def97485e33e616b1d9f55444cf68
chicho <chicho@upn.pe>
2025-04-15 19:18:32 -0500

    Semana4 V1.0

---
+++ Ejercicio4Semana$/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4Semana_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char caracter;
+
+            Console.WriteLine("ingrese caracter");
+            caracter = Console.ReadLine()[0];
+
+            if (caracter == 's')
+            {
+               
+                Console.WriteLine("soltero");
+
+            }
+            if (caracter == 'c')
+            {
+
+                Console.WriteLine("casado");
+
+            }
+            if (caracter == 'v')
+            {
+
+                Console.WriteLine("viudo");
+
+            }
+            if (caracter == 'd')
+            {
+
+                Console.WriteLine("divorsiado");
+
+            }
+            if (caracter !='s' && caracter != 'c' && caracter != 'v' && caracter != 'd')
+            {
+                Console.WriteLine("sin especificar");
+            }
+
+
+        }
+    }
+}
