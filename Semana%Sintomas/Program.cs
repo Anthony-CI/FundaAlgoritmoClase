8502793234ef41c1cb21366c260e7b5740c0aec8
NYGS <nygs@upn.pe>
2025-04-23 15:44:18 -0500

    Semana5 v3.0

---
+++ Semana%Sintomas/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_Sintomas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("hola mundo" + " "+ i);
+            }
+
+            Console.WriteLine("Sintomas de COVID");
+            Console.WriteLine("Usted tiene fiebre (si/no)");
+            string respuesta = Console.ReadLine();
+            if (respuesta=="si")
+            {
+                
+                Console.WriteLine("Usted tiene dolor de cabeza");
+                string respuesta2 = Console.ReadLine();
+                if (respuesta2=="si")
+                {
+                    Console.WriteLine("usted tiene hipogeusia");
+                    String respuesta3 = Console.ReadLine();
+
+                    if (respuesta3=="si")
+                    {
+
+                        Console.Beep(450, 1000);
+                        Console.Beep(900, 2000);
+                        Console.Beep(1000, 3000);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Usted tiene COVID");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Beep(900,2000);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Usted tiene infeccion estomacal");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Usted posiblemnte tenga gripe");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Posiblemente no tengas COVID");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
