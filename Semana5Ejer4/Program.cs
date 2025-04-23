befdbdc2e43b6cace2c6ffd6a86d33bc9760fb45
Anthony <lindemberg2102@gmail.com>
2025-04-22 22:56:56 -0500

    Semana5 V2.0

--- Semana5Ejer4/Program.cs
+++ Semana5Ejer4/Program.cs
@@ -15,12 +15,15 @@ namespace Semana5Ejer4
 
             Console.WriteLine("Ingrese un numero");
             n = Convert.ToInt32(Console.ReadLine());
+            int cuadrados=0;
             int suma = 0;
             for (int i =1; i <= n; i++) 
             {
-                suma = suma +( i * i);
-                Console.WriteLine(suma);
+                cuadrados = i * i;
+                suma += cuadrados ;
+                Console.WriteLine("Los cuadrados son " +" "+cuadrados);
             }
+            Console.WriteLine("La suma es " + suma);
         }
     }
 }
