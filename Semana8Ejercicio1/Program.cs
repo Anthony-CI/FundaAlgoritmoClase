0ecebad89a3c943233dbb6fd0767f036a9ba5d49
chicho <chicho@upn.pe>
2025-05-13 19:18:03 -0500

    Semana8

---
+++ Semana8Ejercicio1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana8Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //primera forma 
+            factorial1();
+
+            Console.WriteLine("*******************");
+            //Segunda forma -> manioular el resultado devuleto por la funcion factorial2
+            BigInteger resultado =factorial2();
+            resultado = resultado / 2;
+            Console.WriteLine("El factorial /2 es: " + resultado);
+
+            Console.WriteLine("*******************");
+            //Tercera forma -> pasar el resultado por valor
+            int n;
+            Console.WriteLine("Ingrese un numero: ");
+            n = int.Parse(Console.ReadLine());
+            factorial3(n);
+
+            Console.WriteLine("*******************");
+
+            //Cuarta forma -> pasar el resultado por valor
+            int x;
+            Console.WriteLine("Ingrese un numero: ");
+            x = int.Parse(Console.ReadLine());
+            BigInteger respuesta = factorial4(x);
+            Console.WriteLine("El factorial es: " + respuesta);
+
+
+        }
+
+        static void factorial1()
+        {
+            int numero;
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+
+                factorial *= i;
+                //factorial= factorial*i;
+            }
+            Console.WriteLine("El factorial es " + factorial);
+        }
+
+        static BigInteger factorial2()
+        {
+            int numero;
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+
+                //factorial *= i;
+                factorial= factorial*i;
+            }
+            return factorial;
+        }
+
+        static void factorial3(int numero)
+        {
+            
+
+            BigInteger factorial = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+
+                //factorial *= i;
+                factorial = factorial * i;
+            }
+            Console.WriteLine("El factorial es " + factorial);
+        }
+
+
+        static BigInteger factorial4(int numero)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+
+                //factorial *= i;
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+
+    }
+}
---
+++ Semana8Practica/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana8Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese sueldo");
+
+        }
+
+        static double  CalculandoSueldo(double sueldo)
+        {
+
+
+            if (sueldo <= 1500)
+            {
+                double porsentaje = (sueldo * 10) / 100;
+                Console.WriteLine("el 10 % es:" + " " + porsentaje);
+                double nuevoSueldo = sueldo - porsentaje;
+                Console.WriteLine("El nuevo sueldo es:" + " " + nuevoSueldo);
+                return nuevoSueldo;
+            }
+            else
+            {
+                if (sueldo > 1500 && sueldo <= 3000)
+                {
+
+                    double nuevoSueldo = sueldo - 1500;
+
+                    double porcentaje = (nuevoSueldo * 5 / 100);
+
+                    Console.WriteLine("el 5 % de impuesto a pgara es " + " " + porcentaje);
+
+                    double nuevoSueldo1 = sueldo - porcentaje;
+
+                    Console.WriteLine("El nuevo sueldo a pagar es:" + " " + nuevoSueldo1);
+
+
+
+                }
+                else
+                {
+
+                    if (sueldo > 3000)
+                    {
+                        double nuevoSueldo = sueldo - 3000;
+
+                        double porcentaje = (nuevoSueldo * 3 / 100);
+
+                        Console.WriteLine("el 3 % de impuesto a pgara es " + " " + porcentaje);
+
+                        double nuevoSueldo1 = sueldo - porcentaje;
+
+                        Console.WriteLine("El nuevo sueldo a pagar es:" + " " + nuevoSueldo1);
+
+                    }
+
+
+                }
+            }
+
+
+            return 0;
+        }
+
+
+
+    }
+}
