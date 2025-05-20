9bd827f1d0d7ca2a219c37ca1bf7da223656e2db
chicho <chicho@upn.pe>
2025-05-20 18:28:32 -0500

    Semna9 v1

---
+++ Semana9Ejercicio1/Areas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana9Ejercicio1
+{
+    class Areas
+    {
+
+        public double AreaCirculo(double r) {
+            
+            double area;
+            area = Math.PI * Math.Pow(r, 2);
+            return area;
+        }
+
+        public double AreaTriangulo(double b, double h) {
+
+            double area = (b * h) / 2;
+            return area;
+        }
+
+        public double AreaRectangulo(double b, double h) {
+
+            return b*h;
+        }
+
+    }
+}
---
+++ Semana9Ejercicio1/Perimetros.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana9Ejercicio1
+{
+    class Perimetros
+    {
+        public double PerimetroCuadrado(double lado)
+        {
+            return lado * 4;
+        }
+
+        public double PerimetroTriangulo(double lado1, double lado2, double lado3)
+        {
+            return lado1 + lado2 + lado3;
+        }
+
+        public double PerimetroCirculo(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+
+        public double PerimetroTrapecio(double baseMayor, double baseMenor, double lado1, double lado2)
+        {
+            return baseMayor + baseMenor + lado1 + lado2;
+        }
+
+        public double PerimetroRombo(double lado)
+        {
+            return lado * 4;
+        }
+
+        public void PerimetroRectangulo(double baseR, double altura, ref double r)
+        {
+            // El ref se usa para que el resultado se almacene en la variable r
+            r = (baseR+altura) * 2;
+        }
+    }
+}
