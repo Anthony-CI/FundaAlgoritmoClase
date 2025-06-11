1ae06736f873bb521cf90dcd05ba0486a9a9bc5c
chicho <chicho@upn.pe>
2025-06-10 19:16:45 -0500

    Semana12Clas1

---
+++ Semana12Clas1/MetodosArreglos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana12Clas1
+{
+    public class MetodosArreglos
+    {
+        string[] listaAlumnos = new string[0];
+
+        //insertar
+        public void Insertar(string nombre)
+        {
+            Array.Resize(ref listaAlumnos, listaAlumnos.Length + 1);
+            int indice = listaAlumnos.Length - 1;
+            listaAlumnos[indice] = nombre;
+        }
+
+        //Mostrar
+
+        public void Mostrar()
+        {
+            foreach (string item in listaAlumnos)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void Eliminar(string nombreBuscar)
+        {
+            for (int i = 0; i < listaAlumnos.Length; i++)
+            {
+                if (listaAlumnos[i] == nombreBuscar)
+                {
+                    //busca la psocion del buscar
+                    for (int j = i; j < listaAlumnos.Length - 1; j++)
+                    {
+                        listaAlumnos[j] = listaAlumnos[j + 1];
+                    }
+                    //Eliminar
+                    Array.Resize(ref listaAlumnos, listaAlumnos.Length - 1);
+                    return;
+                }
+            }
+            Console.WriteLine("No se encontró el nombre a eliminar: " + nombreBuscar);
+
+        }
+    }
+}
---
+++ Semana12Clase1Ejer1Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using Semana12Clas1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana12Clase1Ejer1Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetodosArreglos lista = new MetodosArreglos();
+            lista.Insertar("Juan");
+            lista.Insertar("Maria");
+            lista.Insertar("Pedro");
+            lista.Mostrar();
+            Console.ReadKey();
+            Console.WriteLine("*******************************************");
+            lista.Eliminar("Juan");
+            lista.Mostrar();
+
+
+        }
+    }
+}
