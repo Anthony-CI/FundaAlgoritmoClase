46cc50e15bc7bd57067d5d5eca281161d79adce9
chicho <chicho@upn.pe>
2025-06-24 19:07:36 -0500

    Semana14V1.0

---
+++ Semana14BCClass1Ejer1/ManipulacionCaracteres.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana14BCClass1Ejer1
+{
+    public class ManipulacionCaracteres
+    {
+        public string Encriptar(string str, char llave)
+        {
+            string encriptado = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char L = (char)((int)str[i] + llave);
+                encriptado += L;
+            }
+            return encriptado;
+        }
+
+        public string Desencriptar(string str, char llave)
+        {
+            string encriptado = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char L = (char)((int)str[i] - llave);
+                encriptado += L;
+            }
+            return encriptado;
+        }
+
+        public string ContarVocales(string palabra)
+        {
+            int contador = 0;
+
+            string vocal =palabra.ToLower();
+
+            foreach (char c in vocal)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    contador++;
+                }
+            }
+           
+
+            return "Total de vocales: " + contador;
+        }
+
+        public string Invertir(string str)
+        {
+            string invertido = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                invertido += str[i];
+            }
+            return invertido;
+
+        }
+
+        public string Palindromo(string str)
+        {
+            string invertida = Invertir(str);
+
+            if (str.ToLower() == invertida.ToLower())
+            {
+                return "Palindromo";
+            }
+            else
+            {
+                return "No es un palindromo";
+            }
+        }
+        
+
+    }
+}
---
+++ Semana14ConsoleClass1ejer1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Semana14BCClass1Ejer1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana14ConsoleClass1ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ManipulacionCaracteres mC = new ManipulacionCaracteres();
+            /*
+            char llave;
+            Console.Write("Ingrese el texto a encriptar: ");
+            string txttOriginal = Console.ReadLine();
+            Console.WriteLine("Ingrese llave:  ");
+            llave = char.Parse(Console.ReadLine());
+            string txtEncriptado = mC.Encriptar(txttOriginal, llave);
+            Console.WriteLine("Texto encriptado: " + txtEncriptado);
+            
+            Console.Write("Ingrese su llave:  ");
+            llave = char.Parse(Console.ReadLine());
+            Console.WriteLine("Texto original es " + mC.Desencriptar(txtEncriptado, llave));
+            */
+
+            Console.Write("Ingrese el texto a contar vocales: ");
+            string texto = Console.ReadLine();
+            string respuesta = mC.ContarVocales(texto);
+            Console.WriteLine(respuesta);
+
+            Console.Write("Ingrese el texto a invertir: ");
+            string textoInvertir = Console.ReadLine();
+            string textoInvertido = mC.Invertir(textoInvertir);
+            Console.WriteLine("Texto invertido: " + textoInvertido);
+
+            Console.Write("Ingrese el texto a verificar si es palindromo: ");
+            string textoPalindromo = Console.ReadLine();
+            string resultadoPalindromo = mC.Palindromo(textoPalindromo);
+            Console.WriteLine(resultadoPalindromo);
+        }
+    }
+}
